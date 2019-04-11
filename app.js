fe2ca0ffd8b18efc2779b7f3b85be7133859283f
Atul Upadhyay <atulatulupadhyay337@gmail.com>
2019-04-12 00:15:56 +0530

    added files

--- app.js
+++ app.js
@@ -77,6 +77,6 @@ app.delete("/blogs/:id", function(req, res){
 
 
 //adding the listener
-app.listen(3000, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Blog App has started");
 });
